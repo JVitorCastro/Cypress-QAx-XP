516aeaa1ef7d9577241305627ca2e3325157ba0c
J. Vitor <srvitorcastro@gmail.com>
2023-05-24 12:48:04 -0300

    Implementação do consumo de API

--- projects/shave-xp-cypress/cypress.config.js
+++ projects/shave-xp-cypress/cypress.config.js
@@ -1,9 +1,30 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
+const {Pool} = require('pg');
+const dbConfig = {
+  host: 'motty.db.elephantsql.com',
+  user: 'gtheequl',
+  password: 'ZBCNIjKfD3fpxpkkWgIyo_LFyFzG2A_A',
+  database: 'gtheequl',
+  port: 5432
+}
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        removeUser(email) {
+          return new Promise(function (resolve) {
+            const pool = new Pool(dbConfig)
+            pool.query('DELETE FROM users WHERE email = $1', [email], function (error, result) {
+              if (error) {
+                throw error
+              }
+              resolve({succcess: result})
+            })
+          })
+        }
+      })
     },
     viewportWidth: 1920,
     viewportHeight: 1080,
--- projects/shave-xp-cypress/cypress/e2e/login.cy.js
+++ projects/shave-xp-cypress/cypress/e2e/login.cy.js
@@ -4,58 +4,70 @@ import data from '../fixtures/users-login.json'
 
 describe('login', () => {
 
-    context('Quando submeto o formulário', () => {
-        it('Deve logar com sucesso no sistema', () => {
-            // O código abaixo utiliza fixtures para acessar os dados do teste
-            // cy.fixture('users-login').then(function (data){
-            //     loginPage.submit(data.email, data.password)
-            //     shaversPage.header.userShouldBeLoggedIn(data.name)
-            // })
+  context('Quando submeto o formulário', () => {
+    it('Deve logar com sucesso no sistema', () => {
+      // O código abaixo utiliza fixtures para acessar os dados do teste
+      // cy.fixture('users-login').then(function (data){
+      //     loginPage.submit(data.email, data.password)
+      //     shaversPage.header.userShouldBeLoggedIn(data.name)
+      // })
 
-            loginPage.submit(data.success.email, data.success.password)
-            shaversPage.header.userShouldBeLoggedIn(data.success.name)
+      cy.task('removeUser', data.success.email)
+        .then(function (result){
+          console.log(result)
         })
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/users',
+        body: data.success
+      }).then(function (response){
+        expect(response.status).to.eq(201)
+      })
 
-        it('não deve logar com senha incorreta', () => {
-            loginPage.submit(data.invpass.email, data.invpass.password)
-            loginPage.noticeShouldBe(data.invpass.message)
-
-        })
-
-        it('não deve logar com email não cadastrado',() => {
-
-            loginPage.submit(data.email404.email, data.email404.password)
-            loginPage.noticeShouldBe(data.email404.message)
-        })
-
-        it('campos obrigatórios', () => {
-            loginPage.submit()
-
-            // cy.contains('.alert-error', 'E-mail é obrigatório')
-            //     .should('be.visible')
-            // cy.contains('.alert-error', 'Senha é obrigatória')
-            //     .should('be.visible')
-
-            loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
-        })
+      loginPage.submit(data.success.email, data.success.password)
+      shaversPage.header.userShouldBeLoggedIn(data.success.name)
     })
 
-    context('Senha muito curta', () => {
-        data.shortpass.forEach((p)=> {
-            it(`Não deve logar com a senha: ${p}`, () => {
-                loginPage.submit('teste@gmail.com', p)
-                loginPage.alertShouldBe('Pelo menos 6 caracteres')
-            })
-        })
+    it('não deve logar com senha incorreta', () => {
+      loginPage.submit(data.invpass.email, data.invpass.password)
+      loginPage.noticeShouldBe(data.invpass.message)
+
     })
 
-    context('Email no formato incorreto', () => {
-        data.invemails.forEach((e)=> {
-            it(`Não deve logar com o email: ${e}`, () => {
-                loginPage.submit(e, 'pwd123')
-                loginPage.alertShouldBe('Informe um email válido')
-            })
-        })
+    it('não deve logar com email não cadastrado',() => {
+
+      loginPage.submit(data.email404.email, data.email404.password)
+      loginPage.noticeShouldBe(data.email404.message)
     })
+
+    it('campos obrigatórios', () => {
+      loginPage.submit()
+
+      // cy.contains('.alert-error', 'E-mail é obrigatório')
+      //     .should('be.visible')
+      // cy.contains('.alert-error', 'Senha é obrigatória')
+      //     .should('be.visible')
+
+      loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
+    })
+  })
+
+  context('Senha muito curta', () => {
+    data.shortpass.forEach((p)=> {
+      it(`Não deve logar com a senha: ${p}`, () => {
+        loginPage.submit('teste@gmail.com', p)
+        loginPage.alertShouldBe('Pelo menos 6 caracteres')
+      })
+    })
+  })
+
+  context('Email no formato incorreto', () => {
+    data.invemails.forEach((e)=> {
+      it(`Não deve logar com o email: ${e}`, () => {
+        loginPage.submit(e, 'pwd123')
+        loginPage.alertShouldBe('Informe um email válido')
+      })
+    })
+  })
 
 })
