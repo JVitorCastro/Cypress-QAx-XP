258baac471b37ba65826dfc0dda6b84f6f511cd8
J. Vitor <srvitorcastro@gmail.com>
2023-05-25 11:32:20 -0300

    Validação de API com Joi

--- projects/shave-xp-cypress/api/app.js
+++ projects/shave-xp-cypress/api/app.js
@@ -1,10 +1,20 @@
 const bcrypt = require('bcrypt')
+const Joi = require('joi')
 const express = require('express')
+
+const validator = require('express-joi-validation').createValidator({ passError: true })
 const app = express()
 
 app.use(express.json())
 
 const { deleteUser, insertUser } = require('./db')
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  is_shaver: Joi.boolean().required()
+})
 
 app.get('/welcome', function (req, res) {
 	res.json({ message: 'Olá QAx!' })
@@ -17,7 +27,7 @@ app.delete('/user/:email', async function (req, res) {
 	res.status(204).end()
 })
 
-app.post('/user', async function (req, res) {
+app.post('/user', validator.body(userSchema), async function (req, res) {
 	const { name, email, password, is_shaver } = req.body
 	const hashPass = await bcrypt.hash(password, 8)
 
@@ -42,5 +52,18 @@ app.post('/user', async function (req, res) {
 		res.status(500).json({ error: 'Ocorreu um erro.', stack: error })
 	}
 })
+
+app.use((err, req, res, next) => {
+  if (err && err.error && err.error.isJoi) {
+    // we had a joi error, let's return a custom 400 json response
+    res.status(400).json({
+      type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+      message: err.error.toString()
+    });
+  } else {
+    // pass on to another error handler
+    next(err);
+  }
+});
 
 app.listen(5000)
